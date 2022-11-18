fd591b217f6a80585d1dd5e3b596295420f9a491
natsumi10 <natsumi.k1234@gmail.com>
2022-11-18 17:35:47 +0900

    Add files via upload

---
+++ publish_notification.py
@@ -0,0 +1,11 @@
+from mattermostdriver import Driver
+import requests
+
+
+
+#Mattermost Driver Setting
+md = Driver({
+	'url': 'http://localhost:8065/hooks/1pjm48jmetbejctfhhbuadk7iw'
+	'token': 'ixd1769wcjyr8jymmqffzc748c'
+})
+url = "http://localhost:8080/"
---
+++ publish_notification.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+echo "Hello, World!"
+source /var/lib/venv01/bin/activate
+python3 -V
+deactivate
